abe2d06d9fc36e176b1be594d87feb00a491d89f
Anna Wilson <annie.marie.mason@gmail.com>
2016-06-02 13:49:56 -0700

    pseudocode for clothing methods

--- lib/OpenWeatherMapWrapper.rb
+++ lib/OpenWeatherMapWrapper.rb
@@ -6,9 +6,10 @@ class OpenWeatherMapWrapper
   BASE_URL = "http://api.openweathermap.org/data/2.5/"
 
   def initialize(data)
+    @id = data["weather"][0]["id"]
     @main = data["weather"][0]["main"]
     @desc = data["weather"][0]["description"]
-    @temp = data["main"]["temp"] #this is in Kelvin
+    @temp = data["main"]["temp"]
     @image = "http://openweathermap.org/img/w/#{data["weather"][0]["icon"]}.png"
   end
 
@@ -17,4 +18,34 @@ class OpenWeatherMapWrapper
     self.new(data)
   end
 
+  private
+
+  def find_top(temp)
+    # 60+   = tank top
+    # 50-59 = t-shirt
+    # 40-49 = long sleeve & light jacket
+    # 30-39 = long sleeve & heavy jacket
+    # 29-   = long sleeve & snow jacket
+  end
+
+  def find_bottom(temp)
+    # 60+   = shorts
+    # 50-59 = shorts
+    # 40-49 = leggings
+    # 30-39 = leggings
+    # 29-   = snow pants
+  end
+
+  def find_extras(id,temp)
+    # by id
+    # 200 = thunderstorm
+    # 300 = drizzle
+    # 500 = rain
+    # 600 = snow
+    # 800 = clear
+
+    # by temp
+    # 30-39 = gloves & earwarmers
+    # 29-   = gloves & hat
+  end
 end
