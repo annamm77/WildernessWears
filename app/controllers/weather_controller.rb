4efb43e20aa0b3d4c050908567ef356687a466f0
Anna Wilson <annie.marie.mason@gmail.com>
2016-06-03 21:53:57 -0700

    added outfit view. will display outfit suggestion based on zip

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -6,8 +6,7 @@ class WeatherController < ApplicationController
   def create_outfit
     @wrapper = OpenWeatherMapWrapper.find(params["zipcode"])
     @outfit = Outfit.new(temp: @wrapper.temp, id: @wrapper.id)
-    #establish _weather and _outfit partial info
-    #render weather#showoutfit
+    render "outfit"
   end
 
 end
