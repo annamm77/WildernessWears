0e2006c5c176bd4c53a050f9fe37c0da40222a81
Anna Wilson <annie.marie.mason@gmail.com>
2016-06-04 21:19:08 -0700

    sierra wrapper will return an item's name url and image

--- lib/SierraTradingPostWrapper.rb
+++ lib/SierraTradingPostWrapper.rb
@@ -1,23 +1,21 @@
 require 'httparty'
 
 class SierraTradingPostWrapper
-  attr_reader :id
+  attr_reader :name, :url, :image
 
   BASE_URL = "http://api.sierratradingpost.com/api/1.0/"
 
   def initialize(data)
-    @id = data["weather"][0]["id"]
+    @name = data["Name"]
+    @url = data["WebUrl"]
+    @image = data["Images"]["PrimaryMedium"]
   end
 
   def self.search(term)
-    #replaces spaces with -s
+    #replaces spaces with "-"s.
     term.gsub! /\s+/, '-'
     data = HTTParty.get(BASE_URL + "products/search~#{term}/?api_key=#{ENV["SIERRA_TRADING_POST_KEY"]}").parsed_response
-    binding.pry
-    # data["Result"] array of 24 results.
-    # data["Result"][0]["WebUrl"]
-    # data["Result"][0]["Images"]["PrimaryMedium"]
-    # data["Result"][0]["Name"]
+    data = data["Result"].sample
 
     self.new(data)
   end
